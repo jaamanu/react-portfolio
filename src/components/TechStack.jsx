656a0b3edb03f7a1c70724e2e74e615fa90160f7
jaamanu <jaamanu@gmail.com>
2023-05-24 06:17:06 +0200

    update techstack.jsx file

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const TechStack = ({ techStack }) => {
   return (
     <div className='t-5 flex-center text-center px-4 py-2 bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg'>
